e62d3f1b49183dadf6537e620baab15c688de163
Jirat Nakarit <puen@thebitstudio.com>
2024-06-27 13:22:42 +0700

    update servo pattern

--- include/servo_pattern.h
+++ include/servo_pattern.h
@@ -2,32 +2,59 @@
 #include <ESP32Servo.h>
 
 /*
-* SG90, 0.12s/60deg --> 0.002s/1deg
-* 
+* SG90,     0.12s/60deg --> 0.002s/1deg
+* D2S51,    0.07s/60deg --> 0.001166667s/1deg
+* D2S51,    0.05s/60deg --> 0.000833333s/1deg
 */
 void delayPattern(uint16_t delayTime)
 {
     unsigned long startTime = millis();
     while (millis() - startTime < delayTime)
     {
-        vTaskDelay(1);
+        delay(1);
     }
 }
+
 
 void pattern1(Servo servo)
 {
     // -90, 180ms
-    servo.write(0);
-    delayPattern(180);
+    servo.writeMicroseconds(900);
+    delay(150);
+
     // 0, 180ms
-    servo.write(90);
-    delayPattern(180);
+    servo.writeMicroseconds(1500);
+    delay(150);
+
     // +90, 180ms
-    servo.write(180);
-    delayPattern(180);
+    servo.writeMicroseconds(2100);
+    delay(150);
+
+    // 0, 180ms
+    servo.writeMicroseconds(1500);
+    delay(150);
+
+    // +90, 180ms
+    servo.writeMicroseconds(2100);
+    delay(150);
+
+    // 0, 180ms
+    servo.writeMicroseconds(1500);
+    delay(150);
+
+    // -90, 180ms
+    servo.writeMicroseconds(900);
+    delay(150);
+
+    // 0, 180ms
+    servo.writeMicroseconds(1500);
+    delay(150);
+
     // +360, 720ms
-    servo.write(180);
-    delayPattern(720);
+    servo.writeMicroseconds(2100);
+    delay(420);
     
-    servo.write(90);
+    // 0, 180ms
+    servo.write(1500);
+    delay(150);
 }
