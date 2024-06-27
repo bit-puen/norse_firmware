8a7e8115a4f1bc6e3960417fc00924a1cbb59250
Jirat Nakarit <puen@thebitstudio.com>
2024-06-27 16:41:57 +0700

    add pattern2

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,21 @@
 #include <ESP32Servo.h>
 #include "config/pin.h"
 // #include "servo_pattern.h"
+// #define SERVO_DEG_PER_MS    0.857142857  // D2S51
+// #define SERVO_DEG_PER_MS    0.75  // D2S51
+// #define SERVO_DEG_PER_MS    0.923076923  // D2S51
+#define SERVO_DEG_PER_MS    1.2  // SG90
+#define SERVO_MAX_MSTIME    600
+#define SERVO_MID_MSTIME    1500
+
 
 Servo turnTable;
 
+
+void moveCw90(uint16_t intervalTime, uint16_t degree);
+void moveCcw90(uint16_t intervalTime, uint16_t degree);
 void pattern1();
+void pattern2();
 
 void setup() {
   Serial.begin(115200);
@@ -15,10 +26,38 @@ void setup() {
 
 void loop() 
 {
-  pattern1();
-  delay(1000);
+  // pattern1();
+  pattern2();
+  delay(2000);
 }
 
+void moveCw90(uint16_t intervalTime, uint16_t degree)
+{
+  float goalDegPerMs = (float)degree / (float)intervalTime;
+
+  // Serial.print("goalDegPerMs: ");
+  // Serial.println(goalDegPerMs);
+  // Serial.println(intervalTime);
+
+  int msPwm = SERVO_MID_MSTIME - int((SERVO_MAX_MSTIME / SERVO_DEG_PER_MS) * goalDegPerMs);
+  // uint16_t startTime = millis();
+
+  // Serial.print("msPwm: ");
+  // Serial.println(msPwm);
+  turnTable.writeMicroseconds(msPwm);
+  delay(intervalTime);
+  turnTable.writeMicroseconds(1500);
+}
+
+void moveCcw90(uint16_t intervalTime, uint16_t degree)
+{
+  float goalDegPerMs = (float)degree / (float)intervalTime;
+  int msPwm = SERVO_MID_MSTIME + int((SERVO_MAX_MSTIME / SERVO_DEG_PER_MS) * goalDegPerMs);
+
+  turnTable.writeMicroseconds(msPwm);
+  delay(intervalTime);
+  turnTable.writeMicroseconds(1500);
+}
 
 void pattern1()
 {
@@ -61,4 +100,19 @@ void pattern1()
     // 0, 180ms
     turnTable.write(1500);
     delay(150);
+}
+
+void pattern2()
+{
+  moveCw90(150, 90);
+  delay(150);
+  moveCcw90(150, 90);
+  delay(150);
+  moveCcw90(150, 90);
+  delay(150);
+  moveCw90(150, 90);
+  delay(150);
+  moveCcw90(1000, 360); 
+  moveCcw90(222, 80); // +error
+  delay(150);
 }
