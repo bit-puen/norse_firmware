299b019bf041b71c64bc6af02d2132412444c4da
Jirat Nakarit <puen@thebitstudio.com>
2024-08-09 16:50:34 +0700

    add dynamixel

---
+++ lib/dynamixel/dynamixel.h
@@ -0,0 +1,49 @@
+#include "mbed.h"
+
+////////// EEPROM ///////////
+
+////////// RAM ///////////
+#define REG_TORQUE_ENABLE       0x40
+#define REG_LED_ENABLE          0x41
+#define REG_GOAL_VELOCITY       0x68
+#define REG_PRESENT_VELOCITY    0x80
+
+////////// INSTRUCTION ///////////
+#define PING                    0x01
+#define READ_DATA               0x02
+#define WRITE_DATA              0x03
+#define REBOOT                  0x08
+
+////////// ERROR ///////////
+#define RESULT_FAIL             0x01
+#define INS_ERR                 0x02
+#define CRC_ERR                 0x03
+#define DATA_RANGE_ERR          0x04
+#define DATA_LENGTH_ERR         0x05
+#define DATA_LIMIT_ERR          0x06
+#define ACCESS_ERR              0x07
+
+////////// VARIABLE ///////////
+#define DELAY_TIME_US           3000
+
+class Dynamixel
+{
+    public:
+        Dynamixel(PinName tx, PinName rx, int baudRate, PinName rs485DirectionPin);
+
+        void enableTorque(uint8_t id);
+        void disableTorque(uint8_t id);
+        void enableLed(uint8_t id);
+        void disableLed(uint8_t id);
+        void setGoalVelocity(uint8_t id,uint16_t goalVelocity, uint8_t direction);
+        
+
+    private:
+        BufferedSerial _dynamixel;
+        DigitalOut _rs485DirectionPin;
+        int _baudRate;
+        // void read(uint8_t id, uint16_t registerAddress, uint16_t bytes, uint8_t* data);
+        void write(uint8_t id, uint16_t registerAddress, uint16_t bytes, uint8_t* data);
+        unsigned short update_crc(unsigned short crc_accum, unsigned char* data_blk_prt, unsigned short data_blk_size);
+
+};      
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,229 @@
 #include "mbed.h"
+#include "dynamixel.h"
 
-void main()
+#define MAXIMUM_BUFFER_SIZE     32
+
+
+void mecanumKinematics(uint16_t speed, uint16_t strafe, uint16_t turn);
+void startEngin();
+
+void stopMoving();
+void moveForward(uint16_t velocity);
+void moveBackward(uint16_t velocity);
+void moveLeft(uint16_t velocity);
+void moveRight(uint16_t velocity);
+void turnLeft(uint16_t velocity);
+void turnRight(uint16_t velocity);
+void moveForwardLeftDiagonal(uint16_t velocity);
+void moveForwardRightDiagonal(uint16_t velocity);
+void moveBackwardLeftDiagonal(uint16_t velocity);
+void moveBackwardRightDiagonal(uint16_t velocity);
+
+
+static BufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+// Dynamixel XL430(USBTX, USBRX, 115200, D7);
+Dynamixel XL430(D8, D2, 1000000, D7);
+
+
+FileHandle *mbed::mbed_override_console(int fd)
 {
+    return &uartUsb;
+}
+
+
+int main()
+{
+  uint16_t goalVelocity = 265;
+  ThisThread::sleep_for(5s);
+  startEngin();
+
   while (true)
   {
-    /* code */
-  }
+    moveForward(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
+
+    moveBackward(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
+
+    moveLeft(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
+
+    moveRight(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
+
+    turnLeft(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
+
+    turnRight(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
   
+    moveForwardLeftDiagonal(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
+
+    moveBackwardLeftDiagonal(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
+
+    moveForwardRightDiagonal(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
+
+    moveBackwardRightDiagonal(goalVelocity);
+    ThisThread::sleep_for(2s);
+    stopMoving();
+    ThisThread::sleep_for(2s);
+
+    stopMoving();
+    ThisThread::sleep_for(3s);
+    wait_us(100);
+  }
+  return 0;
+}
+
+// void mecanumKinematics(uint8_t speed, uint8_t strafe, uint8_t turn)
+// {
+//   uint16_t frontLeftSpeed = speed + strafe - turn;
+//   uint16_t frontRightSpeed = speed - strafe - turn;
+//   uint16_t rearLestSpeed = speed - strafe + turn;
+//   uint16_t rearRightSpeed = speed + strafe - turn;
+
+//   XL430.setGoalVelocity(1, 255, 0);
+// }
+
+void mecanumKinematics(uint16_t speed, uint16_t strafe, uint16_t turn)
+{
+
+}
+
+void startEngin()
+{
+  XL430.enableLed(1);
+  XL430.enableLed(2);
+  XL430.enableLed(3);
+  XL430.enableLed(4);
+  XL430.enableTorque(1);
+  XL430.enableTorque(2);
+  XL430.enableTorque(3);
+  XL430.enableTorque(4);
+  ThisThread::sleep_for(1s);
+  // XL430.disableLed(1);
+  // XL430.disableLed(2);
+  // XL430.disableLed(3);
+  // XL430.disableLed(4);
+}
+
+void stopMoving()
+{
+  printf("stop\r\n");
+  XL430.setGoalVelocity(1, 0, 1);
+  XL430.setGoalVelocity(2, 0, 1);
+  XL430.setGoalVelocity(3, 0, 1);
+  XL430.setGoalVelocity(4, 0, 1);
+}
+
+void moveForward(uint16_t velocity)
+{
+  printf("moveForward\r\n");
+  XL430.setGoalVelocity(1, velocity, 0);
+  XL430.setGoalVelocity(2, velocity, 0);
+  XL430.setGoalVelocity(3, velocity, 0);
+  XL430.setGoalVelocity(4, velocity, 0);
+}
+
+void moveBackward(uint16_t velocity)
+{
+  printf("moveBackward\r\n");
+  XL430.setGoalVelocity(1, velocity, 1);
+  XL430.setGoalVelocity(2, velocity, 1);
+  XL430.setGoalVelocity(3, velocity, 1);
+  XL430.setGoalVelocity(4, velocity, 1);
+}
+
+void moveLeft(uint16_t velocity)
+{
+  printf("moveLeft\r\n");
+  XL430.setGoalVelocity(1, velocity, 0);
+  XL430.setGoalVelocity(2, velocity, 1);
+  XL430.setGoalVelocity(3, velocity, 0);
+  XL430.setGoalVelocity(4, velocity, 1);
+}
+
+void moveRight(uint16_t velocity)
+{
+  printf("moveRight\r\n");
+  XL430.setGoalVelocity(1, velocity, 1);
+  XL430.setGoalVelocity(2, velocity, 0);
+  XL430.setGoalVelocity(3, velocity, 1);
+  XL430.setGoalVelocity(4, velocity, 0);
+}
+
+void turnLeft(uint16_t velocity)
+{
+  printf("turnLeft\r\n");
+  XL430.setGoalVelocity(1, velocity, 0);
+  XL430.setGoalVelocity(2, velocity, 1);
+  XL430.setGoalVelocity(3, velocity, 1);
+  XL430.setGoalVelocity(4, velocity, 0);
+}
+
+void turnRight(uint16_t velocity)
+{
+  printf("turnRight\r\n");
+  XL430.setGoalVelocity(1, velocity, 1);
+  XL430.setGoalVelocity(2, velocity, 0);
+  XL430.setGoalVelocity(3, velocity, 0);
+  XL430.setGoalVelocity(4, velocity, 1);
+}
+
+void moveForwardLeftDiagonal(uint16_t velocity)
+{
+  printf("moveForwardLeftDiagonal\r\n");
+  XL430.setGoalVelocity(1, velocity, 0);
+  XL430.setGoalVelocity(2, 0, 1);
+  XL430.setGoalVelocity(3, velocity, 0);
+  XL430.setGoalVelocity(4, 0, 1);
+}
+
+void moveForwardRightDiagonal(uint16_t velocity)
+{
+  printf("moveForwardRightDiagonal\r\n");
+  XL430.setGoalVelocity(1, 0, 1);
+  XL430.setGoalVelocity(2, velocity, 0);
+  XL430.setGoalVelocity(3, 0, 1);
+  XL430.setGoalVelocity(4, velocity, 0);
+}
+
+void moveBackwardLeftDiagonal(uint16_t velocity)
+{
+  printf("moveBackwardLeftDiagonal\r\n");
+  XL430.setGoalVelocity(1, velocity, 1);
+  XL430.setGoalVelocity(2, 0, 1);
+  XL430.setGoalVelocity(3, velocity, 1);
+  XL430.setGoalVelocity(4, 0, 1);
+}
+
+void moveBackwardRightDiagonal(uint16_t velocity)
+{
+  printf("moveBackwardRightDiagonal\r\n");
+  XL430.setGoalVelocity(1, 0, 1);
+  XL430.setGoalVelocity(2, velocity, 1);
+  XL430.setGoalVelocity(3, 0, 1);
+  XL430.setGoalVelocity(4, velocity, 1);
 }
