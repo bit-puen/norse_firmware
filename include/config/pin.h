55ee601a7537fa1f55139f229e56765cf3bb6d20
bit-puen <puen@thebitstudio.com>
2024-08-17 01:15:06 +0700

    add protocol pin

--- include/config/pin.h
+++ include/config/pin.h
@@ -2,8 +2,11 @@
 #define CONFIG_PIN_H
 
 #ifdef MK_I
-#define PIN_DYNAMIXEL_TX    PA_9
-#define PIN_DYNAMIXEL_RX    PA_10
+#define PIN_DYNAMIXEL_TX            PA_9
+#define PIN_DYNAMIXEL_RX            PA_10
+#define PIN_DYNAMIXEL_DIRECTION     PA_8
+#define PIN_PROTOCOL_TX             USBTX
+#define PIN_PROTOCOL_RX             USBRX
 #endif
 
 #endif
