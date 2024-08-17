9f3b2b3bf3f4891d605041c924874bca4c9829bd
bit-puen <puen@thebitstudio.com>
2024-08-18 01:26:22 +0700

    add norsebot register

--- include/norsebot.h
+++ include/norsebot.h
@@ -4,6 +4,7 @@
 #include "config/pin.h"
 #include "dynamixel.h"
 #include "norseprotocol.h"
+#include "norsebotregister.h"
 
 #ifdef MK_I
 #define WHEEL_FRONT_RIGHT_ID        1
@@ -44,15 +45,19 @@ class NorseBot
         ~NorseBot();
 
         void init();
+        void protocolHandler();
+        void commandMovingHandler();
+        void manualModeHandler();
 
+    private:
         void startEngine();
         void stopEngine();
 
         void stopMoving();
         void moveForward(uint16_t speed);
         void moveBackward(uint16_t speed);
-        void moveLeft(uint16_t speed);
-        void moveRight(uint16_t speed);
+        void moveRotateLeft(uint16_t speed);
+        void moveRotateRight(uint16_t speed);
         void moveForwardLeft(uint16_t speed);
         void moveForwardRight(uint16_t speed);
         void moveBackwardLeft(uint16_t speed);
@@ -67,9 +72,15 @@ class NorseBot
         Thread* _protocolThread;
 
         volatile bool readingThreadRunning = false;
+        volatile bool isPacketAvilable = false;
 
         static void protocolThread(void const *pvParamter);
         void protocolThreadWorker();
+
+    private:
+        uint8_t registerControlMode;
+        uint8_t registerModeManualCommand;
+        uint16_t registerModeManualSpeed;
 };
 
 
