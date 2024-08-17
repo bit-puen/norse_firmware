283f6ee1f87874fb916342d7c7c78c0889777ed8
bit-puen <puen@thebitstudio.com>
2024-08-18 01:27:49 +0700

    add ping event

--- lib/norseprotocol/norseprotocol.cpp
+++ lib/norseprotocol/norseprotocol.cpp
@@ -19,12 +19,30 @@ NorsePacket NorseProtocol::getPacket()
 
 bool NorseProtocol::getIsPacketAvilable()
 {
-    return false;
+    return isPacketAvilable;
 }
 
 void NorseProtocol::runCommunication()
 {
     read();
+    if (isPacketAvilable)
+    {
+        eventHandler();
+    }
+}
+
+void NorseProtocol::noticError(uint8_t reasonCode)
+{
+    uint8_t data[] = {reasonCode};
+    generatePacket(EVENT_RESPONSE_ERROR, data, 1);
+    write();
+}
+
+void NorseProtocol::respondEventPing()
+{
+    uint8_t data[] = {PCB_MAJOR_VER, PCB_MINOR_VER, FW_MAJOR_VER, FW_MINOR_VER, PTC_MAJOR_VER, PTC_MINOR_VER};
+    generatePacket(EVENT_RESPONSE_DATA, data, 6);
+    write();
 }
 
 void NorseProtocol::read()
@@ -42,11 +60,11 @@ void NorseProtocol::read()
         {
             // _uart.write(tmpBuffer, readBytesNumber);
 
-            if (tmpBuffer[0] == '\r' || tmpBuffer[0] == 0x0D) 
+            if (tmpBuffer[0] == '\r' || tmpBuffer[0] == STOP_BYTE_1) 
             {
                 isGetReturnChar = true;
             }
-            else if ((tmpBuffer[0] == '\n' || tmpBuffer[0] == 0x0A) && isGetReturnChar) 
+            else if ((tmpBuffer[0] == '\n' || tmpBuffer[0] == STOP_BYTE_2) && isGetReturnChar) 
             {
                 isEnding = true;
                 isGetReturnChar = false;
@@ -70,18 +88,17 @@ void NorseProtocol::read()
         // 2. Check header (start byte and protocol version)
         if (!validateHeader()) 
         {
-            // noticError(DEVICE_F411RE, 0x00);
+            noticError(ERR_HEADER);
             isError = true;
-            printf("Header error \r\n");
+            // printf("Header error \r\n");
         }
 
         // 3. Validate checksum
         if (!validateChecksum(rxBuffer, bytes)) 
         {
-            // noticError(DEVICE_F411RE, 0x00);
+            noticError(ERR_CHECKSUM);
             isError = true;
-            printf("Checksum error \r\n");
-
+            // printf("Checksum error \r\n");
         }
 
         // 4. Create packet
@@ -115,6 +132,34 @@ void NorseProtocol::write()
     _uart.write(txBuffer, sizeof(uint8_t) * lengthTxBuffer);
 }
 
+void NorseProtocol::generatePacket(uint8_t eventId, uint8_t *params, uint8_t bytesParams)
+{
+    uint8_t i;
+    uint8_t iStart = NUMBER_HEADER + 2;  // 2 = LEN + EID
+    uint16_t checksum;
+
+    // 1. Create header
+    txBuffer[0] = START_BYTE_1;
+    txBuffer[1] = START_BYTE_2;
+    // 2. Create lenght
+    txBuffer[2] = bytesParams + 3;  // 3 = EID + 2checksum
+    // 3. Create event ID and params
+    txBuffer[3] = eventId;
+    for (i = iStart; i < iStart + bytesParams; i++)
+    {
+        txBuffer[i] = params[i - iStart];
+    }
+    // 4. Create checksum
+    checksum = calculateChecksum(txBuffer, bytesParams + 1);  // 1 = EID
+    txBuffer[i] = checksum & 0xFF;      // LSB
+    txBuffer[i + 1] = checksum >> 8;    // MSB
+    // 5. Create trailer
+    txBuffer[i + 2] = STOP_BYTE_1;
+    txBuffer[i + 3] = STOP_BYTE_2;
+
+    lengthTxBuffer = i + 4;
+}
+
 bool NorseProtocol::validateChecksum(uint8_t *packet, uint8_t bytes)
 {
     uint8_t length = bytes + NUMBER_HEADER;
@@ -144,11 +189,6 @@ uint16_t NorseProtocol::calculateChecksum(uint8_t *packet, uint8_t bytes)
     return sum;
 }
 
-FileHandle *NorseProtocol::mbed_override_console(int fd)
-{
-    return &_uart;
-}
-
 bool NorseProtocol::validateHeader()
 {
     if ((rxBuffer[0] == START_BYTE_1) && (rxBuffer[1] == START_BYTE_2))
@@ -158,3 +198,18 @@ bool NorseProtocol::validateHeader()
     return false;
 }
 
+FileHandle *NorseProtocol::mbed_override_console(int fd)
+{
+    return &_uart;
+}
+
+void NorseProtocol::eventHandler()
+{
+    switch (_packet.eventId)
+    {
+    case EVENT_PING:    respondEventPing(); break;
+    
+    default:
+        break;
+    }
+}
--- lib/norseprotocol/norseprotocol.h
+++ lib/norseprotocol/norseprotocol.h
@@ -1,8 +1,6 @@
 #include "mbed.h"
 #include "protocolconfig.h"
 
-#define MAXIMUM_BUFFER_SIZE         12
-
 struct NorsePacket
 {
     uint8_t length;
@@ -20,24 +18,32 @@ class NorseProtocol
         bool getIsPacketAvilable();
         void runCommunication();
 
+        void noticError(uint8_t reasonCode);
+
+    private:
+        void respondEventPing();
+
     private:
         BufferedSerial _uart;
         NorsePacket _packet;
 
-        bool isPacketAvilable = true;
+        volatile bool isPacketAvilable = true;
         bool isReading = false;
         uint8_t counter = 0;
         uint8_t errorIndex = 0x00;
         uint8_t lengthTxBuffer = 0;
+        uint8_t lengthRxBuffer = 0;
         uint8_t rxBuffer[MAXIMUM_BUFFER_SIZE];          // Not optimal size
         uint8_t txBuffer[MAXIMUM_BUFFER_SIZE];          // Not optimal size
-        uint8_t parameters[MAXIMUM_BUFFER_SIZE/2];      // Not optimal size
+        // uint8_t parameters[MAXIMUM_BUFFER_SIZE/2];      // Not optimal size
         int _baudRate;
 
         void read();
         void write();
+        void generatePacket(uint8_t eventId, uint8_t *params, uint8_t bytesParams);
         bool validateChecksum(uint8_t *packet, uint8_t bytes);
         bool validateHeader(); 
         uint16_t calculateChecksum(uint8_t *packet, uint8_t bytes);
         FileHandle *mbed_override_console(int fd);
+        void eventHandler();
 };
