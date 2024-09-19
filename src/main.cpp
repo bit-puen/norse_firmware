9b1c07cac63ce92a9857465c4bded22d3e985b6b
bit-puen <puen@thebitstudio.com>
2024-09-19 13:38:04 +0700

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -5,51 +5,15 @@
 
 static const char* TAG = "main_norsebot";
 
-// #include "norseprotocol.h"
-
-// dynamixel_config_t dynamixelConfig{
-//   PIN_DYNAMIXEL_TX,         /* Tx pin */ 
-//   PIN_DYNAMIXEL_RX,         /* Rx pin */ 
-//   PIN_DYNAMIXEL_DIRECTION,  /* Direction pin */ 
-//   BUADRATE_DYNAMIXEL        /* Buad rate */ 
-// };
-// protocol_config_t protocolConfig{
-//   PIN_PROTOCOL_TX,          /* Tx pin */ 
-//   PIN_PROTOCOL_RX,          /* Rx pin */ 
-//   BUADRATE_PROTOCOL         /* Buad rate */ 
-// };
-
-// NorseBot norseBot(&dynamixelConfig, &protocolConfig);
-
-// int main()
-// {
-//   ThisThread::sleep_for(2s);
-//   norseBot.init();
-
-//   while (true)
-//   {
-//     ThisThread::sleep_for(100ms);
-//   }
-  
-//   return 0;
-// }
-
-// TaskHandle_t task;
-// static void testThread(void *pvParamter);
-
 HardwareSerial commandSerial(0);
 HardwareSerial dynamixelSerial(1);
-// NorseBot(commandSerial, dynamixelSerial, GPIO_NUM_4);
 NorseBot norseBot(commandSerial, dynamixelSerial, GPIO_NUM_4);
-
-// NorseProtocol testPTC(commandSerial, 115200);
 
 void setup()
 {
   ESP_LOGI(TAG, "Initial Norsebot");
   norseBot.init();
   delay(2000);
-  // norseBot.autoModeHandler();
 }
 void loop()
 {
